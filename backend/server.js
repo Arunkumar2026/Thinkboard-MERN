d0d51611dd61481a235bdffdfc7d30e1db7770e2
Arun Kumar <akulakumar453@gmail.com>
2026-02-19 17:16:54 +0530

    this is main server file

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,29 @@
 import express from "express"
+import notesRouters from "./routes/notesRoutes.js";
 // const express = require("express");
 
 
 
 const app = express()
 
+app.use("/api/notes", notesRouters);
 
-app.get("/api/notes", (req, res) => {
-    res.status(200).send("you got 5 notes");
-});
 
-app.post("/api/notes", (req, res) => {
-    res.status(201).json({message:"Note created successfully!"})
-});
+// app.get("/api/notes", (req, res) => {
+//     res.status(200).send("you got 5 notes");
+// });
 
-app.put("/api/notes/:id", (req, res) => {
-    res.status(200).json({message:"Note updated successfully!"})
-});
+// app.post("/api/notes", (req, res) => {
+//     res.status(201).json({message:"Note created successfully!"})
+// });
 
-app.delete("/api/notes/:id", (req, res) => {
-    res.status(200).json({message:"Note deleted successfully!"})
-});
+// app.put("/api/notes/:id", (req, res) => {
+//     res.status(200).json({message:"Note updated successfully!"})
+// });
+
+// app.delete("/api/notes/:id", (req, res) => {
+//     res.status(200).json({message:"Note deleted successfully!"})
+// });
 
 app.listen(5001, ()=>{
     console.log("Server started on PORT: 5001");
