ea1feb22b34153be1650c5eeb04d1669a5eabf87
Arun Kumar <akulakumar453@gmail.com>
2026-02-24 15:26:22 +0530

    This is server file

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import notesRouters from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import rateLimiter from "../middleware/rateLimiter.js";
 // const express = require("express");
 
 
@@ -11,6 +12,15 @@ const app = express()
 const PORT = process.env.PORT || 5001
 
 connectDB();
+
+//middleware
+app.use(express.json()) // this middleware will parse JSON bodies: req.body
+app.use(rateLimiter) 
+//our simple custom middleware
+// app.use((req, res, next) => {
+//     console.log(`Req method is ${req.method} & REq URL is ${req.url}`)
+//     next();
+// })
 
 app.use("/api/notes", notesRouters);
 
