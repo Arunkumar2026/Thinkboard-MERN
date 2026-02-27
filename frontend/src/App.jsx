80a916868dbb0071e32631112f2b16be5c4cf2e2
Arun Kumar <akulakumar453@gmail.com>
2026-02-27 18:41:16 +0530

    source folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,8 @@ import toast from "react-hot-toast"
 const App = () => {
   return (
     <div>
-      <button onClick={(() => toast.error("404 error"))}>Click me</button>
+      <button onClick={(() => toast.success("successfully"))} className="text-red-500 p-4 bg-pink-300 mt-3 ml-3" >Success Message</button><br/><br/>
+      <button onClick={(() => toast.error("404 error"))}>Error Message</button><b/>
       <Routes>
         <Route path="/" element={<HomePage/>} />
         <Route path="/create" element={<CreatePage/>} />
