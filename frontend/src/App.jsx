c8e181ff448c07351fdd9f754cfe1d0d1c16459c
Arun Kumar <akulakumar453@gmail.com>
2026-03-03 22:39:31 +0530

    this is app file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ import toast from "react-hot-toast"
 const App = () => {
   return (
     <div data-theme="forest">
-      <button onClick={(() => toast.success("successfully"))} className="text-red-500 p-4 bg-pink-300 mt-3 ml-3 rounded-br-full" >Success Message</button><br/><br/>
+      {/* <button onClick={(() => toast.success("successfully"))} className="text-red-500 p-4 bg-pink-300 mt-3 ml-3 rounded-br-full" >Success Message</button><br/><br/>
       <button onClick={(() => toast.error("404 error"))}>Error Message</button><b/>
       <button className="btn btn-outline">Click Me</button>
       <button className="btn">Button</button>
@@ -17,7 +17,7 @@ const App = () => {
       <button className="btn btn-secondary">Secondary</button>
       <button className="btn btn-accent">Accent</button>
       <button className="btn btn-ghost">Ghost</button>
-      <button className="btn btn-link">Link</button>
+      <button className="btn btn-link">Link</button> */}
       <Routes>
         <Route path="/" element={<HomePage/>} />
         <Route path="/create" element={<CreatePage/>} />
