f56967d94c236a11ae718632c9e350060cf2167d
Arun Kumar <akulakumar453@gmail.com>
2026-03-04 19:41:58 +0530

    this is home page file

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import {useState} from 'react'
 import Navbar from '../components/Navbar'
+import RateLimitedUI from '../components/RateLimitedUI';
 
 const HomePage = () => {
+  const [isRateLimited, setIsRateLimited] = useState(true);
+  const [notes, setNotes] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {}, [])
+
   return (
     <div className='min-h-screen'>
       <Navbar/>
-    </div>
-  )
-}
 
-export default HomePage
+      {isRateLimited && <RateLimitedUI/>}
+    </div>
+  );
+};
+
+export default HomePage;
