5afd285385ccc77577893aaa211c750fc628f97a
Arun Kumar <akulakumar453@gmail.com>
2026-03-04 22:37:33 +0530

    this is home page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,13 +1,20 @@
 import {useState} from 'react'
 import Navbar from '../components/Navbar'
 import RateLimitedUI from '../components/RateLimitedUI';
+import { useEffect } from 'react'
 
 const HomePage = () => {
   const [isRateLimited, setIsRateLimited] = useState(true);
   const [notes, setNotes] = useState([])
   const [loading, setLoading] = useState(true)
 
-  useEffect(() => {}, [])
+  useEffect(() => {
+     const fetchNotes = async () => {
+
+     }
+
+     fetchNotes();
+  }, [])
 
   return (
     <div className='min-h-screen'>
