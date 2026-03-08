06733199e348a4dcb97fdace7b3e20d5d3b5fd78
Arun Kumar <akulakumar453@gmail.com>
2026-03-08 14:22:43 +0530

    this is note detail page file

--- frontend/src/pages/NoteDetailPage.jsx
+++ frontend/src/pages/NoteDetailPage.jsx
@@ -1,6 +1,12 @@
+import { useState } from "react"
 
 
 const NoteDetailPage = () => {
+  const [note,setNote] = useState(null);
+  const [loading,setLoading] = useState(true);
+  const [saving,setSaving] = useState(false);
+
+  
   return (
     <div>NoteDetailPage</div>
   )
