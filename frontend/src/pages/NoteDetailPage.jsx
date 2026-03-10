13e136843e81d644f30891bcfd40c4d4199b7ef4
Arun Kumar <akulakumar453@gmail.com>
2026-03-10 17:45:23 +0530

    this is note detail page file

--- frontend/src/pages/NoteDetailPage.jsx
+++ frontend/src/pages/NoteDetailPage.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react"
 import toast from "react-hot-toast";
-import { useNavigate, useParams } from "react-router";
+import { Link, useNavigate, useParams } from "react-router";
 import api from "../lib/axios";
-import { LoaderIcon } from "lucide-react";
+import { ArrowLeftIcon, LoaderIcon, Trash2Icon } from "lucide-react";
 
 
 const NoteDetailPage = () => {
@@ -31,7 +31,9 @@ const NoteDetailPage = () => {
     fetchNote();
   }, [id]);
 
-  console.log({note})
+  const handleDelete = () => {};
+
+  const handleSave = () => {};
 
   if (loading){
     return (
@@ -41,7 +43,54 @@ const NoteDetailPage = () => {
     )
   }
   return (
-    <div>NoteDetailPage</div>
+    <div className="min-h-screen bg-base-200">
+      <div className="container mx-auto px-4 py-8">
+        <div className="max-w-2xl mx-auto">
+          <div className="flex items-center justify-between mb-6">
+            <Link to="/" className="btn btn-ghost">
+            <ArrowLeftIcon className="h-5 w-5"/>
+            Back to Notes
+            </Link>
+            <button onClick={handleDelete} className="btn btn-error btn-outline">
+              <Trash2Icon className="h-5 w-5"/>
+              Delete Note
+            </button>
+          </div>
+          <div className="card bg-base-100">
+            <div className="card-body">
+              <div className="form-control mg-4">
+                <label className="label">
+                  <span className="label-text">Title</span>
+                </label>
+                <input type="text"
+                placeholder="Note title"
+                className="input input-bordered"
+                value={note.title}
+                onChange={(e) => setNote({ ...note, title: e.target.value })} />
+              </div>
+
+              <div className="form-control mb-4">
+                <label className="label">
+                  <span className="label-text">Content</span>
+                </label>
+                <textarea 
+                placeholder="Write your note here"
+                className="textarea textarea-bordered h-32"
+                value={note.content}
+                onChange={(e) => setNote({ ...note, content: e.target.value })}
+                />
+              </div>
+
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary" disabled={saving} onClick={handleSave} >
+                  {saving? "Saving...": "Save Changes"}
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
